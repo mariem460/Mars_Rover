c279ac8a90026468aa4af4375f0dbec03f67a035
mariem460 <mariem.oueslati.hajjem@gmail.com>
2021-08-02 13:36:32 +0200

    Create endpoint in the node app to return rover info

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -9,10 +9,27 @@ const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+app.get('/rovers', async (req, res) => {
+    try {
+        let manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.query.name}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+
+        let maxDate = manifest.photo_manifest.max_date;
+
+        let photosContainer = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.query.name}/photos?earth_date=${maxDate}&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+
+        let rover = Object.assign({}, manifest.photo_manifest, {photos : undefined}, {recentPhotos: photosContainer.photos})
+
+        res.send(rover)   
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
 
 // example API call
 app.get('/apod', async (req, res) => {
